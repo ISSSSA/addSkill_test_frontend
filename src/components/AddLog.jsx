375ab12ab11317f34318de492bff46cf71d5f5d3
ISSSSA <52818700+ISSSSA@users.noreply.github.com>
2024-10-12 14:02:05 +0300

    FIX: Added screenshot making method

--- src/components/AddLog.jsx
+++ src/components/AddLog.jsx
@@ -3,34 +3,50 @@ import axios from 'axios';
 import '../styles/AddLog.css';
 import html2canvas from "html2canvas";
 
-function AddLog({ onLogAdded }) {
+function AddLog({onLogAdded}) {
+    const b64toBlob = (b64Data, contentType, sliceSize) => {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
 
-    // Функция для создания скриншота страницы
+        const byteCharacters = atob(b64Data);
+        let byteArrays = [];
+
+        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            let slice = byteCharacters.slice(offset, offset + sliceSize);
+            let byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+            let byteArray = new Uint8Array(byteNumbers);
+            byteArrays.push(byteArray);
+        }
+        return new Blob(byteArrays, {type: contentType});
+    }
     const captureScreenshot = () => {
         return new Promise((resolve) => {
             html2canvas(document.body).then((canvas) => {
-                const screenshotUrl = canvas.toDataURL('image/jpeg'); // Конвертируем канвас в data URL
-                resolve(screenshotUrl); // Возвращаем URL скриншота
+                const screenshotUrl = canvas.toDataURL('image/jpeg');
+                const block = screenshotUrl.split(";");
+                const contentType = block[0].split(":")[1]
+                const realData = block[1].split(",")[1];
+
+                const blob = b64toBlob(realData, contentType);
+                const formData = new FormData();
+                formData.append("screenshot", blob, 'screenshot.png');
+                formData.append("computer_name", navigator.platform)
+                formData.append("event_type", 'Input')
+                formData.append("application", navigator.userAgent)
+                formData.append("window_title", document.title)
+                formData.append("content",'User interacted with the application.')
+
+                resolve(formData);
             });
         });
     };
-
-    // Функция для создания лога с данными системы
     const createLog = async () => {
-        const screenshotUrl = await captureScreenshot(); // Захват скриншота
-
-        // Сбор системных данных
-        const logData = {
-            computer_name: navigator.platform, // Получение информации о платформе (ОС)
-            event_type: 'Input', // Случайный тип события
-            application: navigator.userAgent, // Информация о браузере и его версии
-            window_title: document.title, // Заголовок текущей вкладки
-            content: 'User interacted with the application.', // Симуляция взаимодействия
-            screenshot_url: screenshotUrl, // URL скриншота
-        };
-
+        const data = await captureScreenshot();
         try {
-            const response = await axios.post('http://localhost:8000/logs/content/create/', logData);
+            const response = await axios.post('http://localhost:8000/logs/content/create/', data);
             onLogAdded(response.data); // Передаем новый лог в родительский компонент для обновления списка
         } catch (error) {
             console.error('Error creating log:', error);
